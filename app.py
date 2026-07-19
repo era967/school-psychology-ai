df0daaade0f28469ea496ea6cb17d8c86f22f913
era967 <ualihanersajyn@gmail.com>
2026-07-19 13:08:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -202,11 +202,17 @@ else:
                 f_att = st.slider("Сабаққа қатысу көрсеткіші (%):", 30.0, 100.0, 95.0)
                 submit_student = st.form_submit_button("Мәліметтерді бұлттық базаға жіберу", type="primary")
                 if submit_student:
-                    if f_name:
-                        stu_uuid = f"STU_{np.random.randint(1000, 9999)}"db.collection("students_metrics").document(stu_uuid).
-                        set({"fullname": f_name, "grade": f_grade, "lms_login": f_lms,"delay_hours": f_delay, "screen_time": f_screen, "night_act": f_night,"math": f_math, "lang": f_lang, "attendance": f_att})
-                        st.success(f"🎉 {f_name} деректері бұлттық базаға сәтті сақталды!")
-                    else:st.error("Аты-жөнін толтыру міндетті!")
+                                    if f_name:
+                    stu_uuid = f"STU_{np.random.randint(1000, 9999)}"
+                    db.collection("students_metrics").document(stu_uuid).set({
+                        "fullname": f_name, "grade": f_grade, "lms_login": f_lms,
+                        "delay_hours": f_delay, "screen_time": f_screen, "night_act": f_night,
+                        "math": f_math, "lang": f_lang, "attendance": f_att
+                    })
+                    st.success(f"🎉 {f_name} деректері бұлттық базаға сәтті сақталды!")
+                else:
+                    st.error("Аты-жөнін толтыру міндетті!")
+
                         elif teacher_menu == "➕ Үй тапсырмасын қосу (ДЗ)":
                             st.title("➕ Оқушыларға жаңа үй тапсырмасын (ДЗ) бекіту")
                             with st.form("add_hw_form"):
